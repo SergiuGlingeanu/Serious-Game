ea857c4dc238eb4fc9b5d62fd2825e4a2a99c57e
conor6280 <61231065+conor6280@users.noreply.github.com>
2021-03-18 23:04:46 +0000

    ydjydjd

---
+++ Farming Tests/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+
+    public StructureInfo structureInfo;
+
+    [System.Serializable]
+    public class StructureInfo
+    {
+        public string name;
+        public StructureType type;
+        public uint cost;
+        public GameObject prefab;
+        public Stats stats;
+    }
+
+    [System.Serializable]
+    public struct Stats
+    {
+        public byte slurryStorage;
+        public byte cowStorage;
+        public byte moneyGeneration;
+        public byte accidentRatePerAccidentInterval;
+        public PrecautionType[] precautions;
+    }
+
+    [System.Serializable]
+    public enum StructureType
+    {
+        SlurryTank,
+        Shed,
+        WalledShed,
+        FarmHouse
+    }
+
+    [System.Serializable]
+    public enum PrecautionType
+    {
+        Fence,
+        Signs,
+        StrongerFence,
+        EvenStronerFence
+    }
+
+}
